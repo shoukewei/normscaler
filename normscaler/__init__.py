61158c844c2f9bfda132d370665c3d2789648bba
Shouke Wei <shouke.wei@gmail.com>
2022-12-20 14:37:38 -0800

    Update __init__.py

--- normscaler/__init__.py
+++ normscaler/__init__.py
@@ -1,1 +1,6 @@
+from .scaler import MinMaxScaler
+from .scaler import MaxAbsScaler
+from .scaler import RobustScaler
+from .scaler import StandardScaler
+from .scaler import Normalizer
 from .scaler import DecimalScaler
